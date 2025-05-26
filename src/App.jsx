4c554b254fbd064b4b62334eb3499eee2eea59fb
vishalidinesh1998 <vishalidinesh1998@gmail.com>
2025-05-26 10:21:59 +0530

    Update files

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,27 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
-import Student from './Components/students';
-import Favourit from './Components/favourit';
-import { ArrContext } from './Arrcontext';
-import ArrContextProvider from './Arrcontext';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// ✅ Case-sensitive import (Check spelling + capital letters)
+import Student from './Components/Students';
+import Favourit from './Components/Favourit';
 import Navbar from './Components/Navbar';
+
+import ArrContextProvider from './Arrcontext';
+
 function App() {
-
-
   return (
-
-
-
     <ArrContextProvider>
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path="/" exact element={<Student />} />
+          <Route path="/" element={<Student />} />
           <Route path="/FavouritList" element={<Favourit />} />
         </Routes>
       </BrowserRouter>
     </ArrContextProvider>
-
   );
 }
 
